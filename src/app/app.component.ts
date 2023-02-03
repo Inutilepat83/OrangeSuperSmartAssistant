af2f308491bfe6fe370e6547debfaeaccb7557fe
Inutilepat83 <arthur.mariani@caramail.fr>
2023-02-03 14:37:49 +0000

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,104 @@
 import { Component } from '@angular/core';
 
+interface CognitiveService {
+  created_at: String
+  intent: {
+    name: String
+    confidence: String
+  }
+  intents: {
+    intent: String
+    confidence: String
+  }[]
+  message: {
+    text: String
+    cleaned: String
+    sentiment: {
+      negative: String
+      neutral: String
+      positive: String
+    }
+  }
+  response: String
+}
 @Component({
   selector: 'app-root',
-  template: `
-  <div>
-    <div>Hello {{value}}! How can I help you?</div>
-    <input type="text" [(ngModel)]="response" name="response"/>
-    <div>{{botResponse}}</div>
-  </div>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = 'World';
-  response = '';
-  botResponse = 'hey';
- 
-  onSubmit() {
-    this.botResponse = 'Thanks for your input. I will respond soon!';
+  
+  title = 'web-chatbot-interface';
+  start = true;
+  user_name = true;
+
+  chat: {message: String, agent: String}[] = [
+   
+  ]
+
+  public async start_chat() {
+    await this.delay(500);
+    this.chat.push({
+      'message': 'Hello, I\'m a virtual assistant at Magnus Bank. Be very welcome! I hope to help you with whatever you need. What is your name?', 
+      'agent': 'bot'
+    });
+
+    this.start = false;
+  }
+
+  public async set_name(user: any) {
+    await this.delay(500);
+    this.chat.push({
+      'message': 'Well, ' + user.value + ', what you want?',
+      'agent': 'bot'
+    });
+
+    this.user_name = false;
+  }
+
+  public async user_add_message(user: any) {
+
+    this.chat.push({
+      'message': user.value,
+      'agent': 'user'
+    });
+
+    if (this.start) {
+      await this.start_chat();      
+    } else {
+      if (this.user_name) {
+        await this.set_name(user);
+      } else{
+        this.bot_add_message(user);
+      }
+    }
+    user.value = '';
+  }
+
+  public bot_add_message(user: String) {
+    this.getResponse(user).then(res => {
+      this.chat.push({
+        'message': res.response,
+        'agent': 'bot'
+      });
+      console.log(res);
+    });
+  }
+
+  public getResponse(user: any): Promise<CognitiveService> {
+    return fetch('http://127.0.0.1:5000/predict?text=' + user.value)
+            .then(res => res.json())
+            .then(res => {
+                    return res as CognitiveService
+            })
+  }
+
+  public delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-
-
 export class AppModule { }
